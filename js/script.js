6bca9f11f99c06bf92dedfc798916598e70087b4
Salvatore Borgia <sa.borgia3@gmail.com>
2020-12-10 11:31:12 +0100

    inizio footer in vue

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    footer: [
+      {
+        name: "About",
+        categories: [
+          "The Company",
+          "Institutional",
+          "Social & Events",
+          "Innovation",
+          "Environment",
+          "Technology",
+        ],
+      },
+      {
+        name: "Services",
+        categories: [
+          "Audit & Assurance",
+          "Financial Advisory",
+          "Analytics M&A",
+          "Middle Marketing",
+          "Legal Consulting",
+          "Regulatory Risk",
+        ],
+      },
+      {
+        name: "Support",
+        categories: [
+          "Responsibility",
+          "Terms of Use",
+          "ABout Cookies",
+          "Privacy Policy",
+          "Accessibility",
+          "Information",
+        ],
+      },
+    ],
+  },
+});
